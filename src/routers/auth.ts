c7339b1b15527dac9869c400445c3aaf1189472c
nicolas <linze201@gmail.com>
2024-02-23 11:05:32 +0800

    create user validation schema

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -4,10 +4,20 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/create', async (req: CreateUser, res) => {
-  const { email, password, name } = req.body;
-  const user = await User.create({ email, password, name });
-  res.json({ user });
-});
+router.post(
+  '/create',
+  (req, res, next) => {
+    const { email, password, name } = req.body;
+    if (!name.trim()) return res.json({ error: 'Name is missing!' });
+    if (name.trim().length < 3)
+      return res.json({ error: 'Name is too short!' });
+    next();
+  },
+  async (req: CreateUser, res) => {
+    const { email, password, name } = req.body;
+    const user = await User.create({ email, password, name });
+    res.json({ user });
+  }
+);
 
 export default router;
