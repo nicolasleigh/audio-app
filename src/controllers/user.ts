91f3f06cf27058a47eca04087d8446779be21946
nicolas <linze201@gmail.com>
2024-02-23 14:14:01 +0800

    create email template

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,11 +1,13 @@
 import { RequestHandler } from 'express';
 import nodemailer from 'nodemailer';
+import path from 'node:path';
 
 import { CreateUser } from '#/@types/user';
 import User from '#/models/user';
 import { MAILTRAP_PASS, MAILTRAP_USER } from '#/utils/variables';
 import { generateToken } from '#/utils/helper';
 import EmailVerificationToken from '#/models/emailVerificationToken';
+import { generateTemplate } from '#/mail/template';
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -27,10 +29,32 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
     token,
   });
 
+  const welcomeMessage = `Hi ${name}, welcome to my app! Please verify your email by clicking on the link below:`;
+
   transport.sendMail({
     to: user.email,
     from: 'auth@test.com',
-    html: `<h1>Your verification token is ${token}</h1>`,
+    subject: 'Welcome to my app!',
+    html: generateTemplate({
+      title: 'Welcome to my app',
+      message: welcomeMessage,
+      logo: 'cid:logo',
+      banner: 'cid:welcome',
+      link: '#',
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: 'logo.png',
+        path: path.join(__dirname, '../mail/logo.png'),
+        cid: 'logo',
+      },
+      {
+        filename: 'welcome.png',
+        path: path.join(__dirname, '../mail/welcome.png'),
+        cid: 'welcome',
+      },
+    ],
   });
 
   res.status(201).json({ user });
