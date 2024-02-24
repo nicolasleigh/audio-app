3d80235bf9636b9e1b44badc614c50f519121d7e
nicolas <linze201@gmail.com>
2024-02-24 12:46:37 +0800

    create audio schema

---
+++ src/models/audio.ts
@@ -0,0 +1,54 @@
+import { categories, categoriesTypes } from '#/utils/audio_category';
+import { Model, ObjectId, Schema, model, models } from 'mongoose';
+
+export interface AudioDocument {
+  title: string;
+  about: string;
+  owner: ObjectId;
+  file: {
+    url: string;
+    publicId: string;
+  };
+  poster?: {
+    url: string;
+    publicId: string;
+  };
+  likes: ObjectId[];
+  category: categoriesTypes;
+}
+
+const AudioSchema = new Schema<AudioDocument>(
+  {
+    title: { type: String, required: true, trim: true },
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    file: {
+      type: Object,
+      url: String,
+      publicId: String,
+      required: true,
+    },
+    poster: {
+      type: Object,
+      url: String,
+      publicId: String,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    category: {
+      type: String,
+      enum: categories,
+      default: 'Others',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Audio = models.Audio || model('Audio', AudioSchema);
+
+export default Audio as Model<AudioDocument>;
