3250c05d38ee2340f05b160037acd07187ee842a
nicolas <linze201@gmail.com>
2024-02-24 10:14:26 +0800

    use formidable upload files to the local file system

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -16,6 +16,9 @@ import {
   UpdatePasswordSchema,
 } from '#/utils/validationSchema';
 import { Router } from 'express';
+import formidable from 'formidable';
+import path from 'node:path';
+import fs from 'node:fs';
 
 const router = Router();
 
@@ -39,6 +42,32 @@ router.post('/sign-in', validate(SignInValidationSchema), signIn);
 router.get('/is-auth', mustAuth, (req, res) => {
   res.json({
     profile: req.user,
+  });
+});
+
+router.post('/update-profile', async (req, res) => {
+  if (!req.headers['content-type']?.includes('multipart/form-data'))
+    return res.status(422).json({ error: 'Only accepts form-data' });
+
+  const dir = path.join(__dirname, '../public/profiles');
+
+  try {
+    fs.readdirSync(dir);
+  } catch (error) {
+    fs.mkdirSync(dir);
+  }
+
+  const form = formidable({
+    uploadDir: dir,
+    filename(name, ext, part, form) {
+      return Date.now() + '_' + part.originalFilename;
+    },
+  });
+  form.parse(req, (err, fields, files) => {
+    // console.log('fields', fields);
+    // console.log('files', files);
+
+    res.json({ uploaded: true });
   });
 });
 
