ecd5069faadc936b1990c1a1cf382b7c86ad9117
nicolas <linze201@gmail.com>
2024-02-25 10:59:19 +0800

    create getPlaylistByProfile controller

--- src/routers/playlist.ts
+++ src/routers/playlist.ts
@@ -1,5 +1,6 @@
 import {
   createPlaylist,
+  getPlaylistByProfile,
   removePlaylist,
   updatePlaylist,
 } from '#/controllers/playlist';
@@ -29,5 +30,7 @@ router.patch(
 );
 
 router.delete('/', mustAuth, removePlaylist);
+
+router.get('/by-profile', mustAuth, getPlaylistByProfile);
 
 export default router;
