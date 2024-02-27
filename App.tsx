db27b92d2dc49299536d93076dd19128acce1adf
nicolas <linze201@gmail.com>
2024-02-27 22:12:17 +0800

    "Using Custom Path Aliases with TypeScript" has loads of bugs, removed it and installed package: @tsconfig/react-native

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import SignUp from './src/views/auth/SignUp';
 
 export default function App() {
-  return <SafeAreaView style={styles.container}></SafeAreaView>;
+  return <SignUp />;
 }
 
 const styles = StyleSheet.create({
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'module-resolver',
-    [
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          tests: ['./tests/'],
-          '@components': './src/components',
-          '@utils': './src/utils',
-          '@views': './src/views',
-          '@ui': './src/ui',
-          src: './src',
-        },
-      },
-    ],
-  ],
 };
