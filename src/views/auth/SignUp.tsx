85bbcd66aa83fb2c1cbdcb759349c73ebb2fc1a9
nicolas <linze201@gmail.com>
2024-02-27 22:28:32 +0800

    create SignUp screen

--- src/views/auth/SignUp.tsx
+++ src/views/auth/SignUp.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
+import colors from '../../utils/colors';
 
 export default function SignUp() {
   return (
-    <View style={styles.container}>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.formContainer}>
+        <Text style={styles.label}>Name</Text>
+        <TextInput
+          placeholder="John Doe"
+          placeholderTextColor={colors.INACTIVE_CONTRAST}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          placeholder="john@email.com"
+          placeholderTextColor={colors.INACTIVE_CONTRAST}
+          style={styles.input}
+        />
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          placeholder="********"
+          placeholderTextColor={colors.INACTIVE_CONTRAST}
+          style={styles.input}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: colors.PRIMARY,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: colors.SECONDARY,
+    height: 45,
+    borderRadius: 25,
+    color: colors.CONTRAST,
+    padding: 10,
+  },
+  label: {
+    color: colors.CONTRAST,
+  },
+  formContainer: {
+    width: '100%',
+    paddingHorizontal: 15,
+  },
 });
