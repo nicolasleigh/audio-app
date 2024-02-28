e5f7156074e54f81c76f33fc941e7556671271a5
nicolas <linze201@gmail.com>
2024-02-28 16:25:39 +0800

    create sign in screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import SignUp from './src/views/auth/SignUp';
+import SignIn from './src/views/auth/SignIn';
 
 export default function App() {
-  return <SignUp />;
+  // return <SignUp />;
+  return <SignIn />;
 }
 
 const styles = StyleSheet.create({
---
+++ src/views/auth/SignIn.tsx
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import * as yup from 'yup';
+import AuthFormContainer from '../../components/AuthFormContainer';
+import AuthInputField from '../../components/AuthInputField';
+import Form from '../../components/form';
+import SubmitBtn from '../../components/form/SubmitBtn';
+import AppLink from '../../ui/AppLink';
+import PasswordVisibilityIcon from '../../ui/PasswordVisibilityIcon';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
+
+const signinSchema = yup.object({
+  email: yup
+    .string()
+    .trim()
+    .email('Invalid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .trim()
+    .min(6, 'Password is too short')
+    .required('Password is required'),
+});
+
+export default function SignIn() {
+  const [secureEntry, setSecureEntry] = useState(true);
+  return (
+    <Form
+      initialValues={initialValues}
+      validationSchema={signinSchema}
+      onSubmit={values => {
+        console.log(values);
+      }}>
+      <AuthFormContainer heading="Welcome back!">
+        <View style={styles.formContainer}>
+          <AuthInputField
+            name="email"
+            placeholder="john@email.com"
+            label="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            containerStyle={styles.marginBottom}
+          />
+          <AuthInputField
+            name="password"
+            placeholder="********"
+            label="Password"
+            autoCapitalize="none"
+            secureTextEntry={secureEntry}
+            containerStyle={styles.marginBottom}
+            rightIcon={<PasswordVisibilityIcon privateIcon={secureEntry} />}
+            onRightIconPress={() => setSecureEntry(prev => !prev)}
+          />
+          <SubmitBtn title="Sign in" />
+
+          <View style={styles.linkContainer}>
+            <AppLink title="I Lost My Password" />
+            <AppLink title="Sign up" />
+          </View>
+        </View>
+      </AuthFormContainer>
+    </Form>
+  );
+}
+
+const styles = StyleSheet.create({
+  formContainer: {
+    width: '100%',
+    // paddingHorizontal: 15,
+  },
+  marginBottom: {
+    marginBottom: 20,
+  },
+  linkContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+});
