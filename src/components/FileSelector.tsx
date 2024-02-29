5b4d3ebea17b41318d30454c3347738ce5a6c263
nicolas <linze201@gmail.com>
2024-02-29 21:41:01 +0800

    create FileSelector component

---
+++ src/components/FileSelector.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
+import colors from '../utils/colors';
+
+interface Props {
+  icon?: React.ReactNode;
+  btnTitle?: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+export default function FileSelector({icon, btnTitle, style}: Props) {
+  return (
+    <Pressable style={[styles.btnContainer, style]}>
+      <View style={styles.iconContainer}>{icon}</View>
+      <Text style={styles.btnTitle}>{btnTitle}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  btnContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    height: 70,
+    aspectRatio: 1,
+    borderWidth: 2,
+    borderColor: colors.SECONDARY,
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnTitle: {
+    color: colors.CONTRAST,
+    marginTop: 5,
+  },
+});
--- src/views/Upload.tsx
+++ src/views/Upload.tsx
@@ -1,16 +1,50 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../utils/colors';
+import FileSelector from '../components/FileSelector';
 
 interface Props {}
 
 export default function Upload({}: Props) {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 30, color: 'white'}}>Upload</Text>
+      <View style={styles.fileSelectorContainer}>
+        <FileSelector
+          icon={
+            <MaterialCommunityIcons
+              name="image-outline"
+              size={35}
+              color={colors.SECONDARY}
+            />
+          }
+          btnTitle="Select Poster"
+        />
+        <FileSelector
+          icon={
+            <MaterialCommunityIcons
+              name="file-music-outline"
+              size={35}
+              color={colors.SECONDARY}
+            />
+          }
+          btnTitle="Select Audio"
+          style={{marginLeft: 20}}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: 10,
+  },
+  btnContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fileSelectorContainer: {
+    flexDirection: 'row',
+  },
 });
