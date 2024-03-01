9eb5f57a8d7cb82a177526814c499fb5a752eff0
nicolas <linze201@gmail.com>
2024-03-01 13:01:17 +0800

    create new redux slice: notification

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,16 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import authReducer from './auth';
+import notificationReducer from './notification';
 
 export type RootState = ReturnType<typeof store.getState>;
 
+const reducer = combineReducers({
+  auth: authReducer,
+  notification: notificationReducer,
+});
+
 const store = configureStore({
-  reducer: authReducer,
+  reducer,
 });
 
 export default store;
