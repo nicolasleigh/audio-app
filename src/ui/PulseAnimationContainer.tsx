3fa266483659589af8b76ced7c727dd1d45b6f04
nicolas <linze201@gmail.com>
2024-03-01 17:54:38 +0800

    add pulse animation for loading

---
+++ src/ui/PulseAnimationContainer.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function PulseAnimationContainer({children}: Props) {
+  const opacitySharedValue = useSharedValue(1);
+  const opacity = useAnimatedStyle(() => {
+    return {
+      opacity: opacitySharedValue.value,
+    };
+  });
+
+  useEffect(() => {
+    opacitySharedValue.value = withRepeat(
+      withTiming(0.3, {duration: 1000}),
+      -1,
+      true,
+    );
+  }, []);
+
+  return <Animated.View style={opacity}>{children}</Animated.View>;
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {useFetchLatestAudios} from '../hooks/query';
+import PulseAnimationContainer from '../ui/PulseAnimationContainer';
 
 interface Props {}
 
 export default function Home({}: Props) {
   const {data, isLoading} = useFetchLatestAudios();
-  if (isLoading) {
-    return (
-      <View style={styles.container}>
-        <Text style={{color: 'white', fontSize: 25}}>Loading</Text>
-      </View>
-    );
-  }
-
+  // if (isLoading) {
   return (
-    <View style={styles.container}>
-      {data?.map(item => {
-        return (
-          <Text key={item.id} style={{color: 'white', paddingVertical: 10}}>
-            {item.title}
-          </Text>
-        );
-      })}
-    </View>
+    <PulseAnimationContainer>
+      <Text style={{color: 'white', fontSize: 25}}>Loading</Text>
+    </PulseAnimationContainer>
   );
+  // }
+
+  // return (
+  //   <View style={styles.container}>
+  //     {data?.map(item => {
+  //       return (
+  //         <Text key={item.id} style={{color: 'white', paddingVertical: 10}}>
+  //           {item.title}
+  //         </Text>
+  //       );
+  //     })}
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
