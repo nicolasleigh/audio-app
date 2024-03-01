161e3f4afe9c5d4194c086c2132a284403700807
nicolas <linze201@gmail.com>
2024-03-01 21:33:19 +0800

    create addToFavorite functionality

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,28 +5,60 @@ import LatestUploads from '../components/LatestUploads';
 import RecommendedAudios from '../components/RecommendedAudios';
 import OptionsModal from '../components/OptionsModal';
 import colors from '../utils/colors';
+import {AudioData} from '../@types/audio';
+import client from '../api/client';
+import {Keys, getFromAsyncStorage} from '../utils/asyncStorage';
+import catchAsyncError from '../api/catchError';
+import {useDispatch} from 'react-redux';
+import {updateNotification} from '../store/notification';
 
 interface Props {}
 
 export default function Home({}: Props) {
   const [showOptions, setShowOptions] = useState(false);
+  const [selectedAudio, setSelectedAudio] = useState<AudioData>();
+  const dispatch = useDispatch();
+
+  const handleOnFavPress = async () => {
+    if (!selectedAudio) return;
+
+    // send request with the audio id that we want to add to fav
+    try {
+      const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+      const {data} = await client.post(
+        '/favorite?audioId=' + selectedAudio.id,
+        null,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+    } catch (error) {
+      const errorMsg = catchAsyncError(error);
+      dispatch(updateNotification({message: errorMsg, type: 'error'}));
+    }
+    setSelectedAudio(undefined);
+    setShowOptions(false);
+  };
+
+  const handleOnLongPress = (audio: AudioData) => {
+    setSelectedAudio(audio);
+    setShowOptions(true);
+  };
   return (
     <View style={styles.container}>
       <LatestUploads
         onAudioPress={item => {
           console.log(item);
         }}
-        onAudioLongPress={() => {
-          setShowOptions(true);
-        }}
+        onAudioLongPress={handleOnLongPress}
       />
       <RecommendedAudios
         onAudioPress={item => {
           console.log(item);
         }}
-        onAudioLongPress={() => {
-          setShowOptions(true);
-        }}
+        onAudioLongPress={handleOnLongPress}
       />
       <OptionsModal
         visible={showOptions}
@@ -34,12 +66,19 @@ export default function Home({}: Props) {
           setShowOptions(false);
         }}
         options={[
-          {title: 'Add to playlist', icon: 'playlist-music'},
-          {title: 'Add to favorites', icon: 'cards-heart'},
+          {
+            title: 'Add to playlist',
+            icon: 'playlist-music',
+          },
+          {
+            title: 'Add to favorites',
+            icon: 'cards-heart',
+            onPress: handleOnFavPress,
+          },
         ]}
         renderItem={item => {
           return (
-            <Pressable style={styles.optionContainer}>
+            <Pressable onPress={item.onPress} style={styles.optionContainer}>
               <MaterialCommunityIcons
                 size={24}
                 color={colors.PRIMARY}
