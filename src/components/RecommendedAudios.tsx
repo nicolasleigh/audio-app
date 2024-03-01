18ac658744434e1796c802e31d2eb116576ff2d1
nicolas <linze201@gmail.com>
2024-03-01 20:19:25 +0800

    create GridView component to refactor RecommendedAudios component

--- src/components/RecommendedAudios.tsx
+++ src/components/RecommendedAudios.tsx
@@ -1,41 +1,38 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import {useFetchRecommendedAudios} from '../hooks/query';
 import colors from '../utils/colors';
+import GridView from '../ui/GridView';
 
 interface Props {}
 
 export default function RecommendedAudios({}: Props) {
   const {data, isLoading} = useFetchRecommendedAudios();
-  console.log(data);
+
+  const getPoster = (poster?: string) => {
+    return poster ? {uri: poster} : require('../assets/music.png');
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recommend</Text>
-      <View style={{width: '100%', flexDirection: 'row', flexWrap: 'wrap'}}>
-        {data?.map(item => {
+      <GridView
+        col={3}
+        data={data || []}
+        renderItem={item => {
           return (
-            <View style={{width: '33.33%'}} key={item.id}>
-              <View style={{padding: 5}}>
-                <Image
-                  style={{width: '100%', aspectRatio: 1, borderRadius: 7}}
-                  source={{uri: item.poster}}
-                />
-                <Text
-                  numberOfLines={2}
-                  ellipsizeMode="tail"
-                  style={{
-                    color: colors.CONTRAST,
-                    fontWeight: '500',
-                    fontSize: 16,
-                    marginTop: 5,
-                  }}>
-                  {item.title}
-                </Text>
-              </View>
-            </View>
+            <Pressable>
+              <Image style={styles.poster} source={getPoster(item.poster)} />
+              <Text
+                numberOfLines={2}
+                ellipsizeMode="tail"
+                style={styles.audioTitle}>
+                {item.title}
+              </Text>
+            </Pressable>
           );
-        })}
-      </View>
+        }}
+      />
     </View>
   );
 }
@@ -50,4 +47,11 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 15,
   },
+  audioTitle: {
+    color: colors.CONTRAST,
+    fontWeight: '500',
+    fontSize: 16,
+    marginTop: 5,
+  },
+  poster: {width: '100%', aspectRatio: 1, borderRadius: 7},
 });
---
+++ src/ui/GridView.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+interface Props<T> {
+  data: T[];
+  renderItem(item: T): JSX.Element;
+  col?: number;
+}
+
+export default function GridView<T extends object>({
+  data,
+  renderItem,
+  col = 2,
+}: Props<T>) {
+  return (
+    <View style={styles.container}>
+      {data.map((item, index) => {
+        return (
+          <View key={index} style={{width: `${100 / col}%`}}>
+            <View style={{padding: 5}}>{renderItem(item)}</View>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {width: '100%', flexDirection: 'row', flexWrap: 'wrap'},
+});
