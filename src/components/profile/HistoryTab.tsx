4ab3fef4c447f3bcc72c7ec733c437a1533f2eaf
nicolas <linze201@gmail.com>
2024-03-02 12:46:24 +0800

    create UploadsTab, PlaylistTab, FavoriteTab, and HistoryTab

---
+++ src/components/profile/HistoryTab.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {}
+
+export default function HistoryTab({}: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 20, color: 'white'}}>UploadTab</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,16 +1,47 @@
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import FavoriteTab from '../components/profile/FavoriteTab';
+import HistoryTab from '../components/profile/HistoryTab';
+import PlaylistTab from '../components/profile/PlaylistTab';
+import UploadsTab from '../components/profile/UploadsTab';
+import colors from '../utils/colors';
+
+const Tab = createMaterialTopTabNavigator();
 
 interface Props {}
 
 export default function Profile({}: Props) {
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: styles.tabBarStyle,
+          tabBarLabelStyle: styles.tabBarLabelStyle,
+        }}>
+        <Tab.Screen name="Uploads" component={UploadsTab} />
+        <Tab.Screen name="Playlist" component={PlaylistTab} />
+        <Tab.Screen name="Favorites" component={FavoriteTab} />
+        <Tab.Screen name="History" component={HistoryTab} />
+      </Tab.Navigator>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  tabBarStyle: {
+    backgroundColor: 'transparent',
+    elevation: 0, // remove shadow on Android
+    shadowRadius: 0, // remove shadow on iOS
+    shadowColor: 'transparent',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0,
+  },
+  tabBarLabelStyle: {
+    color: colors.CONTRAST,
+    fontSize: 12,
+  },
 });
