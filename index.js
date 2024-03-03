631c3456c8f4fe521ac47eaaa03c6eb9ed068100
nicolas <linze201@gmail.com>
2024-03-03 11:08:00 +0800

    register audio playback service

--- index.js
+++ index.js
@@ -5,5 +5,9 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import playbackService from './src/playbackService';
+
+TrackPlayer.registerPlaybackService(() => playbackService);
 
 AppRegistry.registerComponent(appName, () => App);
