8cc143ba24aa3bf322085b0a5dbbe7cf2e44f117
nicolas <linze201@gmail.com>
2024-03-03 13:49:35 +0800

    create redux slice: player; create audio play and pause functionality

--- src/hooks/useAudioController.ts
+++ src/hooks/useAudioController.ts
@@ -1,5 +1,11 @@
-import TrackPlayer, {Track} from 'react-native-track-player';
+import TrackPlayer, {
+  Track,
+  usePlaybackState,
+  State,
+} from 'react-native-track-player';
 import {AudioData} from '../@types/audio';
+import {useDispatch, useSelector} from 'react-redux';
+import {getPlayerState, updateOnGoingAudio} from '../store/player';
 
 const updateQueue = async (data: AudioData[]) => {
   const lists: Track[] = data.map(item => {
@@ -17,11 +23,38 @@ const updateQueue = async (data: AudioData[]) => {
 };
 
 const useAudioController = () => {
+  const playbackState = usePlaybackState();
+  const {onGoingAudio} = useSelector(getPlayerState);
+  const dispatch = useDispatch();
+  console.log(playbackState);
+
+  // const isPlayerReady = playbackState.state !== State.None;
+  // const isPlayerReady = playbackState.state === State.Ready;
+
   const onAudioPress = async (item: AudioData, data: AudioData[]) => {
-    await updateQueue(data);
-    const index = data.findIndex(audio => audio.id === item.id);
-    await TrackPlayer.skip(index);
-    await TrackPlayer.play();
+    // console.log(playbackState);
+    // if (isPlayerReady) {
+    if (!playbackState.state) {
+      await updateQueue(data);
+      const index = data.findIndex(audio => audio.id === item.id);
+      await TrackPlayer.skip(index);
+      await TrackPlayer.play();
+      dispatch(updateOnGoingAudio(item));
+    }
+    if (playbackState.state === State.Playing && onGoingAudio?.id === item.id) {
+      // same audio is already playing, then pause it
+      return await TrackPlayer.pause();
+    }
+    if (playbackState.state === State.Paused && onGoingAudio?.id === item.id) {
+      return await TrackPlayer.play();
+    }
+    if (playbackState.state) {
+      await updateQueue(data);
+      const index = data.findIndex(audio => audio.id === item.id);
+      await TrackPlayer.skip(index);
+      await TrackPlayer.play();
+      dispatch(updateOnGoingAudio(item));
+    }
   };
 
   return {onAudioPress};
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import authReducer from './auth';
 import notificationReducer from './notification';
+import playerReducer from './player';
 
 export type RootState = ReturnType<typeof store.getState>;
 
 const reducer = combineReducers({
   auth: authReducer,
   notification: notificationReducer,
+  player: playerReducer,
 });
 
 const store = configureStore({
