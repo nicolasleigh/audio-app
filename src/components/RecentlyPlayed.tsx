9b17687e417961c1ba3aacf8af846b97ced3d934
nicolas <linze201@gmail.com>
2024-03-04 19:32:38 +0800

    add loading skeleton to RecentlyPlayed component

--- src/components/RecentlyPlayed.tsx
+++ src/components/RecentlyPlayed.tsx
@@ -4,11 +4,37 @@ import {useFetchRecentlyPlayed} from '../hooks/query';
 import colors from '../utils/colors';
 import RecentlyPlayedCard from '../ui/RecentlyPlayedCard';
 import GridView from '../ui/GridView';
+import PulseAnimationContainer from '../ui/PulseAnimationContainer';
 
 interface Props {}
 
+const dummyData = new Array(4).fill('');
+
 export default function RecentlyPlayed({}: Props) {
   const {data, isLoading} = useFetchRecentlyPlayed();
+  if (isLoading) {
+    return (
+      <PulseAnimationContainer>
+        <View style={styles.dummyTitleView} />
+        <GridView
+          data={dummyData}
+          renderItem={() => {
+            return (
+              <View
+                style={{
+                  height: 50,
+                  backgroundColor: colors.INACTIVE_CONTRAST,
+                  borderRadius: 5,
+                  marginBottom: 10,
+                }}
+              />
+            );
+          }}
+        />
+      </PulseAnimationContainer>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Recently Played</Text>
@@ -43,6 +69,13 @@ export default function RecentlyPlayed({}: Props) {
 
 const styles = StyleSheet.create({
   container: {},
+  dummyTitleView: {
+    height: 20,
+    width: 150,
+    backgroundColor: colors.INACTIVE_CONTRAST,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
   title: {
     color: colors.CONTRAST,
     fontSize: 20,
